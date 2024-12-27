aaab7c2aae18a7d7f2997b1c889f460cc86524dd
rafaySP <gamingretro919@gmail.com>
2024-12-28 04:38:00 +0500

    update feature added..

--- CRUD.c
+++ CRUD.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+int initialize(int arr[3][3]);
+int update(int arr[3][3]);
 int initialize(int arr[3][3])
 {
     for (int i = 0; i < 3; i++)
@@ -9,10 +11,21 @@ int initialize(int arr[3][3])
         }
     }
 }
+int update(int arr[3][3])
+{
+    int ij[2];
+    printf("Enter the ixj to update");
+    scanf("%d %d",&ij[0],&ij[1]);
+    int updatedelement;
+    printf("Enter the value to update: ");
+    scanf("%d",&updatedelement);
+    arr[ij[0]][ij[1]]=updatedelement;
+}
 int main()
 {
     int arr[3][3];
     initialize(arr);
+    update(arr);
     printf("%d",arr[2][2]);
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 this is going to be a CRUD project on c with an array
 1,  Initialize (4:20 am -> 4:28 am)
+2,  Update (4:32 am -> 4:37 am)
